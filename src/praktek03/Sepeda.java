e6e513d8f65db8550966149f9512c0d35c56362d
panduwo <hudhapandu@gmail.com>
2023-05-28 04:52:32 +0800

    Set Atribut Private

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -1,8 +1,8 @@
 package praktek03;
 public class Sepeda {
-    String Merk;
-    String Warna;
-    double Harga;
+    private String Merk;
+    private String Warna;
+    private double Harga;
     
     void cetakInfo(){
         System.out.println("Merk \t : " + Merk + "\n" +

727aff8fbb50add448b41219495fddb41ca95ff7
panduwo <hudhapandu@gmail.com>
2023-05-28 04:46:07 +0800

    Buat Method Cetak Info

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -1,6 +1,12 @@
 package praktek03;
-public class Sepeda{
+public class Sepeda {
     String Merk;
     String Warna;
     double Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t : " + Merk + "\n" +
+                           "Warna \t : " + Warna + "\n" +
+                           "Harga \t : " + Harga);
+    }
 }
